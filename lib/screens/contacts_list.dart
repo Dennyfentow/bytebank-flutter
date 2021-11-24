306c87dd3257f3bc30e5245e15e62c2e46f46617
Daniel dos Santos <daniel@automastersat.com.br>
2021-11-24 13:52:16 -0300

    parte04 - Finalizando com loading e em caso de erro

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -19,25 +19,39 @@ class ContactsList extends StatelessWidget {
             .then((value) => findAll()),
         builder: (context, snapshot) {
           final List<Contact>? contacts = snapshot.data;
-          if (contacts != null) {
-            // em caso de nulo
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final Contact contact = contacts[index];
-                return _ContactItem(contact);
-              },
-              itemCount: contacts.length,
-            );
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              // em caso de nulo
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact? contact =
+                      contacts != null ? contacts[index] : null;
+                  if (contact != null) {
+                    return _ContactItem(contact);
+                  } else {
+                    return const Text('Sem valores');
+                  }
+                },
+                itemCount: contacts != null ? contacts.length : 0,
+              );
           }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                CircularProgressIndicator(),
-              ],
-            ),
-          );
+
+          return const Text('Unknown Error');
         },
       ),
       floatingActionButton: FloatingActionButton(
